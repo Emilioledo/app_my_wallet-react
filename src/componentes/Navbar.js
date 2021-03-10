d8ddcae7b118992712478e78301cd1af6ab5151c
Emilioledo <fernandezledoemilio@gmail.com>
2021-03-10 18:22:39 -0300

    React

---
+++ src/componentes/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+
+    estiloNavbar () {
+        return {
+            lineHeight: '2',
+            borderRadius: '10px',
+            background: '#253030',
+            padding: '20px',
+            width: '100%',
+            height: '70px',
+            float: 'left',
+            margin: '0em 0 3em 0',
+            padding: '0',
+            listStyle: 'none'
+        }
+    }
+
+    horizontalLi (){
+        return {
+            float: 'left',
+        }  
+    }
+
+    render() {
+        return (
+            <div>
+              <ul style={this.estiloNavbar()} id="nav">
+                  /*botones*/
+              </ul>
+            </div>
+        )
+    }
+}
